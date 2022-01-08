2f66a9388d1873f659759783afffa16e117094e1
MaS <mas@MaSs-MacBook-Pro.local>
2022-01-09 01:10:29 +0200

    Added Success Message component

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,6 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <x-validation-errors class="mb-4" :errors="$errors" />
+                    <x-success-message class="mb-4" />
                     <form method="POST" action="{{ route('profile.update') }}">
                         @method('PUT')
                         @csrf
